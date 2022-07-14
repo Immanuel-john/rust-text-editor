6f3e26ba22d5a2ac77a3175ca4b3f50abe2d9058
Immanuel-john <imjjohn18@gmail.com>
2022-07-14 13:55:09 +0530

    match for error handling

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,29 @@ use std::io::{self, Read, stdin, stdout};
 use termion::raw::IntoRawMode;
 fn main() {
     let _stdoout = stdout().into_raw_mode().unwrap();
+    for b in io::stdin().bytes() {
+        match b {
+            Ok(b) => {
+                let c = b as char;
+                if c.is_control() {
+                    println!("{:?}\r",b)
+                } else {
+                    println!("{:?} ({})\r",b,c)
+                }
+                if b == to_ctrl_byte('q') {
+                    break;
+                }
+            }
+            Err(err) => die(err),
+        }
+    }   
+}
 
-    for b in stdin().bytes() {
-        let b = b.unwrap();
-        let c = b as char;
-    
-        if c.is_control() {
-            println!("{:?} \r",b)
-        }
-        else {
-            println!("{:?} ({})", b, c)
-        }
-        if c == 'q' {
-            break;
-        }
-    }
+fn to_ctrl_byte(c: char) -> u8 {
+    let byte = c as u8;
+    byte           
+}
+
+fn die(e: std::io::Error) {
+    panic!("{}",e)
 }
