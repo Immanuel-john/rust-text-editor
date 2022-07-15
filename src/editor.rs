960735a6979bd4bd22e1b647f5f8adffe7c4ce7f
Immanuel-john <imjjohn18@gmail.com>
2022-07-15 10:13:59 +0530

    Editor struct created

---
+++ src/editor.rs
@@ -0,0 +1,38 @@
+use std::io::{self, stdout};
+use termion::raw::IntoRawMode;
+use termion::event::Key;
+use termion::input::TermRead;
+
+pub struct Editor {}
+
+impl Editor {
+    pub fn run(&self) {
+        let _stdoout = stdout().into_raw_mode().unwrap();
+        for key in io::stdin().keys() {
+            match key {
+                Ok(key) => {
+                    match key {
+                       Key::Char(c) => {
+                            if c.is_control() {
+                                println!("{:?}\r", c as u8)
+                            } else {
+                                println!("{:?} ({})\r",c as u8,c)
+                            }
+                        }
+                        Key::Ctrl('q') => break,
+                        _ => println!("{:?}\r", key),
+                    }
+                }
+                Err(err) => die(err),
+            }
+        }  
+    }
+    pub fn default() -> Self{
+        Editor{}
+    }
+}
+
+
+fn die(e: std::io::Error) {
+    panic!("{}",e)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,6 @@
-use std::io::{self, Read, stdin, stdout};
-use termion::raw::IntoRawMode;
-use termion::event::Key;
-use termion::input::TermRead;
-
+mod editor;
+use editor::Editor;
 fn main() {
-    let _stdoout = stdout().into_raw_mode().unwrap();
-    for key in io::stdin().keys() {
-        match key {
-            Ok(key) => {
-                match key {
-                   Key::Char(c) => {
-                        if c.is_control() {
-                            println!("{:?}\r", c as u8)
-                        } else {
-                            println!("{:?} ({})\r",c as u8,c)
-                        }
-                    }
-                    Key::Ctrl('q') => break,
-                    _ => println!("{:?}\r", key),
-                }
-            }
-            Err(err) => die(err),
-        }
-    }   
-}
-
-fn to_ctrl_byte(c: char) -> u8 {
-    let byte = c as u8;
-    byte           
-}
-
-fn die(e: std::io::Error) {
-    panic!("{}",e)
+   let editor = Editor::default();
+   editor.run();
 }
